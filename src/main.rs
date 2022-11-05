66a0841bfc980ca26d987e6109ee3f51081977b8
Jakob Schlichting <jakob.schlichting@mail.de>
2022-11-05 22:54:35 +0100

    feat: created basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, get, Responder, HttpResponse};
+use anyhow::Result;
+use log::{info};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+    info!("Application starting up...");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
+    return Ok(());
+}
+
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    return HttpResponse::Ok().body("hello world");
 }
